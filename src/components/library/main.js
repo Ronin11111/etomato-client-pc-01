47d35ad8e77706264e681b55a3126ecf483feed8
木木 <2092169005@qq.com>
2023-12-12 15:06:44 +0800

    首页完成

--- src/components/library/main.js
+++ src/components/library/main.js
@@ -1,6 +1,7 @@
 import XtxSkeleton from './xtx-skeleton.vue'
 import XtxCarousel from './xtx-carousel.vue'
 import XtxMore from './xtx-more.vue'
+import defaultImg from '@/assets/images/200.png'
 
 // 封装全局插件
 export default {
@@ -8,5 +9,32 @@ export default {
     app.component(XtxSkeleton.name, XtxSkeleton)
     app.component(XtxCarousel.name, XtxCarousel)
     app.component(XtxMore.name, XtxMore)
+    // 注册全局指令
+    usedirective(app)
   }
+}
+
+// 全局指令
+const usedirective = (app) => {
+  app.directive('lazyImg', {
+    // 在mounted阶段才可获取元素ref
+    mounted (el, binding) {
+      const observe = new IntersectionObserver(([{ isIntersecting }]) => {
+        if (isIntersecting) {
+          // 关闭观察
+          observe.unobserve(el)
+          // 监听图片加载失败,显示默认图片
+          el.onerror = () => {
+            el.src = defaultImg
+          }
+          // 加载成功，将图片地址赋值给图片src
+          el.src = binding.value
+        }
+      }, {
+        threshold: 0
+      })
+      // 开启观察
+      observe.observe(el)
+    }
+  })
 }
