423c439c50953681bf522c01e83b23987eecc5b6
木木 <2092169005@qq.com>
2023-12-15 12:13:36 +0800

    商品详情-面包屑和图片预览组件

---
+++ src/views/Goods/index.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class='xtx-goods-page'>
+    <div class="container" v-if="goods">
+      <!-- 面包屑 -->
+      <XtxBread>
+        <XtxBreadItem to="/">首页</XtxBreadItem>
+        <XtxBreadItem :to="'/category/'+goods.categories[0].id">{{goods.categories[0].name}}</XtxBreadItem>
+        <XtxBreadItem :to="'/category/sub/'+goods.categories[1].id">{{goods.categories[1].name}}</XtxBreadItem>
+        <XtxBreadItem>{{goods.name}}</XtxBreadItem>
+      </XtxBread>
+      <!-- 商品信息 -->
+      <div class="goods-info">
+        <div class="media">
+          <!-- 图片预览组件 -->
+          <GoodsImage :images="goods.mainPictures"></GoodsImage>
+        </div>
+        <div class="spec"></div>
+      </div>
+      <!-- 商品推荐 -->
+      <GoodsRelevant/>
+      <!-- 商品详情 -->
+      <div class="goods-footer">
+        <div class="goods-article">
+          <!-- 商品+评价 -->
+          <div class="goods-tabs"></div>
+          <!-- 注意事项 -->
+          <div class="goods-warn"></div>
+        </div>
+        <!-- 24热榜+专题推荐 -->
+        <div class="goods-aside"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import GoodsRelevant from './components/goods-relevant'
+import GoodsImage from './components/goods-images.vue'
+import { findGoods } from '@/api/product'
+import { watch, ref, nextTick } from 'vue'
+import { useRoute } from 'vue-router'
+export default {
+  name: 'XtxGoodsPage',
+  components: { GoodsRelevant, GoodsImage },
+  setup () {
+    // 调用获取商品信息的函数
+    const goods = getGoodsInfo()
+    return { goods }
+  }
+}
+// 获取商品信息
+const getGoodsInfo = () => {
+  const goods = ref(null)
+  const route = useRoute()
+  // 动态监听路由变化
+  watch(() => route.params.id, (newVal) => {
+    if (newVal && route.path === `/product/${newVal}`) {
+      findGoods(route.params.id).then(({ result }) => {
+      // 每次更新数据前，先将数据值为null，v-if即会销毁和重新创建数据
+        goods.value = null
+        nextTick(() => {
+          goods.value = result
+        })
+      })
+    }
+  }, { immediate: true })
+  return goods
+}
+</script>
+
+<style scoped lang='less'>
+.goods-info {
+  min-height: 600px;
+  background: #fff;
+  display: flex;
+  .media {
+    width: 580px;
+    height: 600px;
+    padding: 30px 50px;
+  }
+  .spec {
+    flex: 1;
+    padding: 30px 30px 30px 0;
+  }
+}
+.goods-footer {
+  display: flex;
+  margin-top: 20px;
+  .goods-article {
+    width: 940px;
+    margin-right: 20px;
+  }
+  .goods-aside {
+    width: 280px;
+    min-height: 1000px;
+  }
+}
+.goods-tabs {
+  min-height: 600px;
+  background: #fff;
+}
+.goods-warn {
+  min-height: 600px;
+  background: #fff;
+  margin-top: 20px;
+}
+</style>
